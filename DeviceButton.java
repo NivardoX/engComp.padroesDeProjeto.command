bc0b02a67ab222008f2b55fb446af34cb8f29b0c
Nivardo Albuquerque <>
2021-04-20 22:50:12 +0000

    DEV - Add command dp

---
+++ DeviceButton.java
@@ -0,0 +1,25 @@
+public class DeviceButton{
+	
+	Command theCommand;
+	
+	public DeviceButton(Command newCommand){
+		
+		theCommand = newCommand;
+		
+	}
+	
+	public void press(){
+		
+		theCommand.execute();
+		
+	}
+	
+	// Now the remote can undo past commands
+	
+	public void pressUndo(){
+		
+		theCommand.undo();
+		
+	}
+	
+}
